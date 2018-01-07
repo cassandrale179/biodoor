1ccb2db5d6ef8a5ac04ce1806821f098c2c5e23c
lenguyentri1999 <michaelle7312@gmail.com>
2018-01-07 10:59:35 -0500

    added text to speech

---
+++ ttsTest.py
@@ -0,0 +1,17 @@
+# # from gtts import gTTS
+# # import os
+# # cwd = os.getcwd()
+# # tts = gTTS(text='Hello', lang='en', slow=True)
+# # tts.save(os.path.join(cwd, "hello.mp3"))
+# from gtts import gTTS
+# import os
+# # def tts(doorStatus, ownername):
+# # text_tts = 'Welcome Minh';
+# tts = gTTS(text='Motherfucker', lang='en')
+# tts.save("welcome.mp3")
+# # os.system("mpg321 welcome.mp3")
+
+import win32com.client as wincl
+speak = wincl.Dispatch("SAPI.SpVoice")
+str_speech = "Welcome home " + name
+speak.Speak(str_speech)
